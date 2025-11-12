dc0c89d587c46a68663bd239e3a7beb9bd6dabfe
kepsgurih <kepsgurih@pm.me>
2025-11-12 09:38:46 +0700

    :wrench: adding login

--- app/(app)/profile/page.tsx
+++ app/(app)/profile/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { Settings, LogOut, User } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function ProfilePage() {
     const user = {
@@ -18,34 +19,36 @@ export default function ProfilePage() {
 
     return (
         <div className="p-6">
-            <div className="flex flex-col items-center text-center mb-6">
-                <Image
-                    src={user.avatar}
-                    alt={user.name}
-                    width={96}
-                    height={96}
-                    unoptimized
-                    className="rounded-full border-4 border-blue-500 shadow-md mb-3"
-                />
-                <h1 className="text-xl font-semibold dark:text-white">{user.name}</h1>
-                <p className="text-sm text-muted-foreground dark:text-white">{user.email}</p>
-            </div>
-
-            <div className="space-y-3">
-                {menuItems.map(({ name, icon: Icon, action }, i) => (
-                    <div
-                        key={i}
-                        onClick={action}
-                        className="flex items-center justify-between p-4 rounded-xl border shadow-sm dark:bg-slate-800 dark:text-white hover:bg-slate-400/10 transition cursor-pointer"
-                    >
-                        <div className="flex items-center space-x-3">
-                            <Icon className="w-5 h-5 text-blue-500" />
-                            <span className="font-medium">{name}</span>
-                        </div>
-                        <span className="text-gray-400">›</span>
+            <SignedOut>
+                <div className="w-full">
+                    <div className="text-center mx-auto bg-white dark:slate-100/20 rounded py-2 dark:hover:bg-slate-400 cursor-pointer">
+                        <SignInButton />
                     </div>
-                ))}
-            </div>
+                </div>
+            </SignedOut>
+            <SignedIn>
+                <div className="flex flex-col items-center text-center mb-6">
+                    <UserButton />
+                    <h1 className="text-xl font-semibold dark:text-white">{user.name}</h1>
+                    <p className="text-sm text-muted-foreground dark:text-white">{user.email}</p>
+                </div>
+
+                <div className="space-y-3">
+                    {menuItems.map(({ name, icon: Icon, action }, i) => (
+                        <div
+                            key={i}
+                            onClick={action}
+                            className="flex items-center justify-between p-4 rounded-xl border shadow-sm dark:bg-slate-800 dark:text-white hover:bg-slate-400/10 transition cursor-pointer"
+                        >
+                            <div className="flex items-center space-x-3">
+                                <Icon className="w-5 h-5 text-blue-500" />
+                                <span className="font-medium">{name}</span>
+                            </div>
+                            <span className="text-gray-400">›</span>
+                        </div>
+                    ))}
+                </div>
+            </SignedIn>
         </div>
     );
 }
